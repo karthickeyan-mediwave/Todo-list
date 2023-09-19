ee7e5035e990cf58a227e62bb9c3bddaee2bb41a
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-09-19 16:36:58 +0530

    script added for check and uncheck

--- app/script.js
+++ app/script.js
@@ -1,51 +1,42 @@
-mytodo = [
-  {
-    id: 1,
-    note: "kkkk",
-  },
-  {
-    id: 2,
-    note: "jjjj",
-  },
-  {
-    id: 3,
-    note: "jjj",
-  },
-  {
-    id: 4,
-    note: "jjjj",
-  },
-];
-
+mytodo = [];
+mytodocheck = [];
 function makeTodoDiv(list) {
+  // list div
   const div = document.createElement("div");
   div.setAttribute("class", "list-card");
-
-  // Configure the check box
-  var checkBox = document.createElement("input");
+  const kk = document.querySelector("#todo-list-checked");
+  //  check box
+  let checkBox = document.createElement("input");
   checkBox.type = "checkbox";
+  checkBox.checked = false;
   checkBox.setAttribute("id", `checkbox ${list["id"]}`);
   checkBox.addEventListener("click", function jj() {
     let v = document.getElementsByTagName("li");
-    for (let i = 0; i < v.length; i += 1) {
-      v.item(i).classList.toggle("checked");
-    }
+    uncheck();
   });
-
-  // Configure the label
+  function uncheck() {
+    if (checkBox.checked == true) {
+      li.style.textDecoration = "line-through";
+      kk.appendChild(ul);
+      console.log("check");
+    } else {
+      checkBox.checked == false;
+      li.style.textDecoration = "none";
+      div.appendChild(ul);
+      console.log("hiii");
+    }
+  }
+  //  label
   var label = document.createElement("label");
   var labelText = document.createElement("span");
   labelText.innerText = list["note"];
-
+  //  list
   const ul = document.createElement("ul");
   ul.setAttribute("class", "ul-class");
   const li = document.createElement("li");
-  // li.setAttribute("class", "list-class");
-  li.setAttribute("id", `list${list["id"]}`);
-
-  // li.innerText = list["note"];
-  // console.log(list["note"]);
-
+  li.setAttribute("class", "list-class");
+  li.setAttribute("id", `list--${list["id"]}`);
+  // append
   label.appendChild(checkBox);
   label.appendChild(labelText);
   ul.appendChild(li);
@@ -58,11 +49,12 @@ function appendToApp(listDiv) {
   const app = document.querySelector("#todo-list");
   app.appendChild(listDiv);
 }
-
+// clear
 function clearApp() {
   const app = document.querySelector("#todo-list");
   app.innerHTML = "";
 }
+// update
 function updateUI() {
   clearApp();
   for (let i = 0; i < mytodo.length; i++) {
@@ -70,6 +62,7 @@ function updateUI() {
     appendToApp(listDiv);
   }
 }
+// button disable
 const inputField = document.getElementById("inputField");
 const submitButton = document.getElementById("submitButton");
 inputField.addEventListener("input", function () {
@@ -79,24 +72,37 @@ inputField.addEventListener("input", function () {
     submitButton.disabled = true;
   }
 });
+// input add
 submitButton.addEventListener("click", function () {
-  // const todolist = {
-  //   id: new Date().getTime(),
-  //   list: list,
-  // };
+  const inputField = document.getElementById("inputField");
+  const list = inputField.value;
+  const todolist = {
+    id: new Date().getTime(),
+    note: list,
+  };
+  addNotes(todolist);
   submitButton.disabled = true;
   inputField.value = "";
 });
+// add function
+function addNotes(todolist) {
+  mytodo.push(todolist);
+  saveToLocalStorage();
+  updateUI();
+}
+// save to local storage
 function saveToLocalStorage() {
   const str = JSON.stringify(mytodo);
   localStorage.setItem("my-todo-list", str);
 }
+// get from local
 function getFromLocalStorage() {
   const str = localStorage.getItem("my-todo-list");
   if (!str) {
-    MyNotes = [];
+    mytodo = [];
   } else {
-    MyNotes = JSON.parse(str);
+    mytodo = JSON.parse(str);
   }
 }
+getFromLocalStorage();
 updateUI();
